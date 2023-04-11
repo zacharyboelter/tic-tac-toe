0eb3092e074bab0dee06008a9778332db9e36bab
Zachary Boelter <zboelter@gmail.com>
2023-04-11 15:26:07 -0700

    attach handleClick

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,15 @@
 
 
 function Square({ value }) {
-  return <button className="square">{value}</button>
+  function handleClick() {
+    console.log('Clicked dat ho!')
+  }
+  return <button
+    className="square"
+    onClick={handleClick}
+  >
+    {value}
+  </button>
 }
 
 
@@ -9,19 +17,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value='1'/>
-        <Square value='2'/>
-        <Square value='3'/>
+        <Square value='1' />
+        <Square value='2' />
+        <Square value='3' />
       </div>
       <div className="board-row">
-        <Square value='4'/>
-        <Square value='5'/>
-        <Square value='6'/>
+        <Square value='4' />
+        <Square value='5' />
+        <Square value='6' />
       </div>
       <div className="board-row">
-        <Square value='1'/>
-        <Square value='1'/>
-        <Square value='1'/>
+        <Square value='7' />
+        <Square value='8' />
+        <Square value='9' />
       </div>
     </>
   )
