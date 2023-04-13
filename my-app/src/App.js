2bb03020942cffd0b45a47ceb7b71c552377dab5
Zachary Boelter <zboelter@gmail.com>
2023-04-12 18:25:33 -0700

    set square values to array indexes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -29,19 +29,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value='1' />
-        <Square value='2' />
-        <Square value='3' />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value='4' />
-        <Square value='5' />
-        <Square value='6' />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value='7' />
-        <Square value='8' />
-        <Square value='9' />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   )
