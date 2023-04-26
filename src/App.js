6e5bedf76b7238b0aa55aceaeafde8d4d5d2e662
Zachary Boelter <zboelter@gmail.com>
2023-04-26 13:13:10 -0700

    remove state from xIsNext

--- src/App.js
+++ src/App.js
@@ -53,22 +53,23 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  // const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)])
   // const currentSquares = history[history.length - 1]
   const [currentMove, setCurrentMove] = useState(0)
   const currentSquares = history[currentMove]
+  const xIsNext = currentMove % 2 === 0
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
     setHistory(nextHistory)
     setCurrentMove(nextHistory.length - 1)
-    setXIsNext(!xIsNext)
+    // setXIsNext(!xIsNext)
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove)
-    setXIsNext(nextMove % 2 === 0)
+    // setXIsNext(nextMove % 2 === 0)
   }
 
   const moves = history.map((squares, move) => {
