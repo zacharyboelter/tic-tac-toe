f4bf8ff2ec6ce8a091b370227c9e09c8ba7288a0
Zachary Boelter <zboelter@gmail.com>
2023-04-26 13:11:00 -0700

    update Game comp. to render current move instead of final move

--- src/App.js
+++ src/App.js
@@ -55,8 +55,9 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)])
-  const currentSquares = history[history.length - 1]
+  // const currentSquares = history[history.length - 1]
   const [currentMove, setCurrentMove] = useState(0)
+  const currentSquares = history[currentMove]
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
